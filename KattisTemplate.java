b54b7303471865b260ded62f1d934b300ddde802
deyixtan <deyixtan@gmail.com>
2020-09-26 14:00:37 +0800

    Added Kattis efficient solution template

---
+++ KattisTemplate.java
@@ -0,0 +1,96 @@
+import java.io.*;
+import java.util.*;
+
+public class KattisTemplate {
+
+    public static void main(String args[]) throws Exception {
+        // Fast IO
+        FastIO sc = new FastIO(System.in);
+        PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+
+        // Inputs
+
+
+        // Logic
+
+
+        // Outputs
+
+
+    }
+
+    // ############################################################################
+    // ########################### Fast IO found online ###########################
+    // https://github.com/MrinallU/USACO-java-fastIO/blob/master/NewIOtemplate.java
+    // ############################################################################
+    static class FastIO {
+        InputStream dis;
+        byte[] buffer = new byte[1 << 17];
+        int pointer = 0;
+
+        public FastIO(String fileName) throws Exception {
+            dis = new FileInputStream(fileName);
+        }
+
+        public FastIO(InputStream is) throws Exception {
+            dis = is;
+        }
+
+        int nextInt() throws Exception {
+            int ret = 0;
+            byte b;
+            do {
+                b = nextByte();
+            } while (b <= ' ');
+            boolean negative = false;
+            if (b == '-') {
+                negative = true;
+                b = nextByte();
+            }
+            while (b >= '0' && b <= '9') {
+                ret = 10 * ret + b - '0';
+                b = nextByte();
+            }
+            return (negative) ? -ret : ret;
+        }
+        
+        long nextLong() throws Exception {
+            long ret = 0;
+            byte b;
+            do {
+                b = nextByte();
+            } while (b <= ' ');
+            boolean negative = false;
+            if (b == '-') {
+                negative = true;
+                b = nextByte();
+            }
+            while (b >= '0' && b <= '9') {
+                ret = 10 * ret + b - '0';
+                b = nextByte();
+            }
+            return (negative) ? -ret : ret;
+        }
+        
+        byte nextByte() throws Exception {
+            if (pointer == buffer.length) {
+                dis.read(buffer, 0, buffer.length);
+                pointer = 0;
+            }
+            return buffer[pointer++];
+        }
+        
+        String next() throws Exception {
+            StringBuffer ret = new StringBuffer();
+            byte b;
+            do {
+                b = nextByte();
+            } while (b <= ' ');
+            while (b > ' ') {
+                ret.appendCodePoint(b);
+                b = nextByte();
+            }
+            return ret.toString();
+        }
+    }
+}
